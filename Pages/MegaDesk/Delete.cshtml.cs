dfa8b5720819638eb8291700d269a7994cd69fba
Adam Ulrich <adamulrich@hotmail.com>
2023-06-10 15:01:28 -0700

    week 8 final final

--- Pages/MegaDesk/Delete.cshtml.cs
+++ Pages/MegaDesk/Delete.cshtml.cs
@@ -29,7 +29,12 @@ namespace MegaDesk_Razor_ACZ.Pages.MegaDesk
                 return NotFound();
             }
 
-            var deskquote = await _context.DeskQuote.FirstOrDefaultAsync(m => m.Id == id);
+           var deskquote = await _context.DeskQuote
+                .Include(q => q.ProductionSpeedCost)
+                .Include(q => q.Desk)
+                .Include(q => q.Desk.Material)
+                .FirstOrDefaultAsync(q => q.Id == id);
+
 
             if (deskquote == null)
             {
